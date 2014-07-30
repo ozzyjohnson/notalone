0827c13e6001beb908abb5c46da3bdea3c66be5c
Aaron Snow <aaronsnow@gmail.com>
2014-07-29 23:50:43 -0400

    Site updated at 2014-07-30 03:50:43 UTC

---
+++ static/js/key-terms.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".key-terms dt").addClass("clickable").addClass("term-inactive");
+  
+  $(".key-terms dt").wrapInner(function(){
+      return "<button title='Expand term'></button>";
+    });
+
+  $(".key-terms dt").click(function() {
+    //$(this).next("dd").toggle();
+    $(this).toggleClass("term-inactive");
+  });
+  
+  if(window.location.hash) {
+    var hash = window.location.hash; //Puts hash in variable, and removes the # character
+   $(hash).removeClass("term-inactive");
+  }
+});
